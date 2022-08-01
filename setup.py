d12c62e362e83ba9101d99a5954ed12d637be207
SemiConductorShortage <83015532+SemiConductorShortage@users.noreply.github.com>
2022-08-02 01:12:32 +0400

    forgot to add a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='UIMaker',
+      version='1.0.0',
+      description='Discord Bot UI Prototyping tool!',
+      author='@SemiConductorShortage on Github',
+      author_email='pastrikk00@gmail.com',
+      packages=['UIMaker'],
+     )
